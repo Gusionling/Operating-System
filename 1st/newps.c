5b0a7c5158dcd0054d2cf9c999c3f1222f7948a5
Gusionling <isndhk@naver.com>
2023-09-12 23:58:00 +0900

    add : TTY

--- 1st/newps.c
+++ 1st/newps.c
@@ -31,7 +31,47 @@ uid_t current_uid;
 char current[TTY_LEN];
 char myPath[PATH_LEN];  //자신의 path
 char myTTY[TTY_LEN];  //자신의 tty
+   
+
+
+void getTTY(char path[PATH_LEN], char tty[TTY_LEN])
+{
+    char statFilePath[PATH_LEN];
+    snprintf(statFilePath, PATH_LEN, "%s/stat", path);
+
+    FILE *fp = fopen(statFilePath, "r");
+    if(fp ==NULL) {
+        perror("Failed to open stat file");
+        exit(EXIT_FAILURE);
+    }
+
+    char statLine[PATH_LEN];
+    if (fgets(statLine, PATH_LEN, fp) ==NULL){
+        perror("Failed to read stat line");
+        exit(EXIT_FAILURE);
+    }
     
+    fclose(fp);
+    
+    // statLine에서 tty 번호 추출
+    char *start = statLine;
+    char *end = strchr(start, ' '); // 첫 번째 공백을 찾음
+    if(end == NULL){
+        perror("Failed to parse TTY number");
+        exit(EXIT_FAILURE);
+    }
+    start = end + 1;
+
+    end = strchr(start, ' ');
+    if(end == NULL){
+        perror("Failed to parse TTY number");
+        exit(EXIT_FAILURE);
+    }
+    
+    strncpy(tty, start, end - start);
+    tty[end -start] = '\0';
+}
+
 int main(int argc, char *argv){
 	
     //현재 pid
@@ -45,7 +85,9 @@ int main(int argc, char *argv){
     strcat(myPath, pidPath);
     
     printf("myPath : %s\n", myPath);
-
+    
+    getTTY(myPath, myTTY);
+    printf("TTY:%s\n", myTTY);
     return 0;
 
 }
