0d87edac975ccb8ccf668a1a2655b8fb30684bff
Gusionling <isdnhk@naver.com>
2023-11-08 22:04:25 +0900

    add : 파일 입력과 출력 구현

--- 4th/vm.c
+++ 4th/vm.c
@@ -2,6 +2,7 @@
 #include <unistd.h>
 #include <string.h>
 #include <stdlib.h>
+#include <time.h>
 
 #define EBIT 262144 //18bit
 #define NBIT 524288 //19bit
@@ -18,6 +19,53 @@ int pnum; //페이지 개수
 int fnum; //프레임 개수
 
 
+
+
+
+
+void createFile(){
+    srand(time(NULL)); // 난수 생성기 시드 초기화
+    
+    FILE *file = fopen("input.in", "w"); //파일 열기 (덮어쓰기 모드)
+
+    if(file == NULL){
+        perror("파일 열기 실패");
+    }
+
+    for(int i=0; i<ADDRESSC; i++){
+        int virtualAdd = rand() % vlength;
+
+        fprintf(file, "%d\n", virtualAdd);
+    }
+
+    fclose(file);
+}
+
+
+void fifo(FILE *inputFile){
+    FILE *outFile = fopen("fifo.out", "w");
+
+    if(outFile == NULL){
+        perror("파일 열기 실패");
+    }
+
+    fprintf(outFile, "%-7s %-8s \n", "No.", "V.a");
+    
+    int virtualAdd;
+    int addressCount = 0;
+
+    while(fscanf(inputFile, "%d", &virtualAdd) == 1){
+        addressCount++;
+        fprintf(outFile, "%-7d %-8d\n", addressCount, virtualAdd);
+    }
+
+    fclose(outFile);
+
+}
+
+
+
+
 int main()
 {
     int select1, select2, select3, select4, select5;
@@ -32,6 +80,7 @@ int main()
             break;
         } else if(select1 == 3) {
             vlength = TBIT;
+            printf("%d\n", vlength);
             break;
         }
         else{
@@ -104,22 +153,52 @@ int main()
         
         printf("E. 가상주소 스트링 입력방식을 선택하시오\n (1. input.in 자동 생성  2. 기존 파일 사용) : ");
         scanf("%d", &select5);
+        
+        // input.in파일 생성
         if(select5 == 1){
             
+            createFile();
 
-        } else if(select5 == 2)  {
-            char filename[100];
-            printf("입력 파일 이름을 입력하시오:");
-            scanf("%s", filename);
+            FILE *inputFile = fopen("input.in", "r");
 
-            FILE *file = fopen(filename, "r");
-
-            if(file == NULL){
-                perror("기존 파일 열기 실패");
+            if(inputFile == NULL){
+                perror("파일 열기 실패");
             }
 
-            fclose(file);
 
+            fifo(inputFile);
+
+
+            fclose(inputFile);
+            break;
+
+        // 기존 파일 사용
+        } else if(select5 == 2)  {
+            
+            char filename[100];
+            FILE *inputFile;
+
+            while(1){
+                printf("입력 파일 이름을 입력하시오:");
+                scanf("%s", filename);
+
+                inputFile = fopen(filename, "r"); //파일 열기 (읽기 모드)
+
+                if(inputFile == NULL){
+                    perror("파일 열기 실패");
+                    printf("파일이 존재하지 않거나 열 수 없습니다. 다시 입력해주세요.\n");
+                }else{
+                    break; 
+                }
+
+            }
+
+            fifo(inputFile);
+
+
+            fclose(inputFile);
+
+            break;
         }
         else{
             printf("올바른 입력이 아닙니다. 다시 입력하세요 \n");
