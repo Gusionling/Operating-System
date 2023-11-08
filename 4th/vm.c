f4046473ae35d5a7693d4ee899ec12efc893bebc
Gusionling <isdnhk@naver.com>
2023-11-08 10:37:02 +0900

    add : 파일 수정

--- 4th/vm.c
+++ 4th/vm.c
@@ -3,17 +3,28 @@
 #include <string.h>
 #include <stdlib.h>
 
-
-
+#define EKB 262144 //18bit
+#define NKB 524288 //19bit
+#define TKB 1048576 //20bit
+#define ADDRESSC 5000 //가상주소 개수 
 
 
 int main()
 {
+    int length;
+
     while(1){
         int select1;
         printf("A. Simulation에 사용할 가상주소 길이를 선택하시오 (1. 18bits  2. 19bits 3. 20bits) : ");
         scanf("%d", &select1);
-        if(select1 == 1 || select1 == 2 || select1 == 3) {
+        if(select1 == 1){
+            length = EKB;
+            break;
+        } else if(select1 == 2){
+            length = NKB;
+            break;
+        } (select1 == 3) {
+            length = TKB;
             break;
         }
         else{
@@ -65,8 +76,20 @@ int main()
         int select5;
         printf("E. 가상주소 스트링 입력방식을 선택하시오\n (1. input.in 자동 생성  2. 기존 파일 사용) : ");
         scanf("%d", &select5);
-        if(select5 == 1 || select5 == 2) {
-            break;
+        if(select5 == 1){
+            
+
+        } else if(select5 == 2)  {
+            char fileNmae[100];
+            printf("입력 파일 이름을 입력하시오:");
+            scanf("%s", fileNmae);
+
+            FILE *file = fopen(filename, "r");
+
+            if(file == NULL){
+                perror("기존 파일 열기 실패");
+            }
+
         }
         else{
             printf("올바른 입력이 아닙니다. 다시 입력하세요 \n");
